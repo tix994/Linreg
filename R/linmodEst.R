cd8ed3a9a8f2323a3c71a4877ed2e5b7eaa6845d
tix994 <txia@hsph.harvard.edu>
2017-10-31 13:39:11 -0400

    added space, closed #1

---
+++ R/linmodEst.R
@@ -0,0 +1,34 @@
+#' Linear regression
+#'
+#' Runs an OLS regression not unlike \code{\link{lm}}
+#'
+#' @param y response vector (1 x n)
+#' @param X covariate matrix (p x n) with no intercept
+#'
+#' @return A list with 4 elements: coefficients, vcov, sigma, df
+#'
+#' @examples
+#' data(mtcars)
+#' X <- as.matrix(mtcars[, c("cyl", "disp", "hp")])
+#' y <- mtcars[, "mpg"]
+#' linreg(y, X)
+#'
+#' @export
+#'
+linmodEst <- function(x, y) {
+  ## CC: crossprod or a QR decomposition (as in the original version) are more efficient
+  coef <- solve(t(x) %*% x) %*% t(x) %*% y
+  print(coef)
+  ## degrees of freedom and standard deviation of residuals
+  df <- nrow(x) - ncol(x)
+  sigma2 <- sum((y - x %*% coef) ^ 2) / df
+  ## compute sigma^2 * (x’x)^-1
+  vcov <- sigma2 * solve(t(x) %*% x)
+  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  list(
+    coefficients = coef,
+    vcov = vcov,
+    sigma = sqrt(sigma2),
+    df = df
+  )
+}
